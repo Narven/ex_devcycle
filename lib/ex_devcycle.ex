3eb7e6016cdc3858844ff0df1cf87141cece89e9
Pedro Luz <pedromsluz@gmail.com>
2022-10-26 18:26:12 +0100

    Initial

---
+++ lib/ex_devcycle.ex
@@ -0,0 +1,18 @@
+defmodule ExDevcycle do
+  @moduledoc """
+  Documentation for `ExDevcycle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDevcycle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_devcycle_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDevcycleTest do
+  use ExUnit.Case
+  doctest ExDevcycle
+
+  test "greets the world" do
+    assert ExDevcycle.hello() == :world
+  end
+end
